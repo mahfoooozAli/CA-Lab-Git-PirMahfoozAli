a8a5cdca406a2bae06c77215d0c64bf1e429034f
mahfoooozAli <pa11848@st.habib.edu.pk>
2026-09-05 16:52:00 +0500

    Update memory operations in Task4a.s
    Refactor memory store and load instructions for clarity.

--- Task4.s
+++ Task4.s
@@ -3,18 +3,19 @@
 
 main:
 
-    li x10, 0x78786464
-    li x11, 0xA8A81919
-    li x5, 256
-    li x6, 0x1F0
+   li x10, 0x78786464
+   li x11, 0xA8A81919
 
-    sw x10, 0(x5)
-    sw x11, 0(x6)
-
-    lhu x12, 0(x5)
-    lh  x13, 0(x6)
-
-    lb  x14, 0(x6)
+   
+   sw x10, 0x100(x0)
+   
+   sw x11, 0x1F0(x0)
+   
+   lhu x12, 0x100(x0)
+   
+   lh  x13, 0x1F0(x0)
+   
+   lb  x14, 0x1F0(x0)
 
 end:
     j end
