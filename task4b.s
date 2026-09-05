0a081b2c2cfc5adf4369b968a372c5245f447161
mahfoooozAli <pa11848@st.habib.edu.pk>
2026-09-05 15:12:28 +0500

    Add comments to clarify assembly code operations

--- task4b.s
+++ task4b.s
@@ -11,23 +11,23 @@ main:
 
     lb x10, 0(x5)
     lh x11, 0(x6)
-    add x12, x10 , x11
-    sw x12, 0(x7)
+    add x12, x10 , x11 # temp = a[0] + b[0]
+    sw x12, 0(x7)   # c[0] = a[0] + b [0]
 
     lb x10, 1(x5)
     lh x11, 2(x6)
-    add x12, x10 , x11
-    sw x12, 4(x7)
+    add x12, x10 , x11 # temp = a[1] + b[1]
+    sw x12, 4(x7)  # c[1] = a[1] + b [1]
 
     lb x10, 2(x5)
     lh x11, 4(x6)
-    add x12, x10 , x11
-    sw x12, 8(x7)   
+    add x12, x10 , x11 # temp = a[2] + b[2]
+    sw x12, 8(x7)   # c[2] = a[2] + b [2]
 
     lb x10, 3(x5)
     lh x11, 6(x6)
-    add x12, x10 , x11
-    sw x12, 12(x7)
+    add x12, x10 , x11  # temp = a[3] + b[3]
+    sw x12, 12(x7) # c[3] = a[3] + b [3]
 
 
 end:
